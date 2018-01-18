2e6d019a9d6bf540cdc0a50490aa78b6d48658a5
Augustas <augustas@sender.lt>
2018-01-18 15:41:29 +0200

    Added install and uninstall methods in SenderPrestashopModule

--- senderprestashop.php
+++ senderprestashop.php
@@ -6,6 +6,10 @@ if (!defined('_PS_VERSION_')) {
  
 class SenderPrestashop extends Module
 {
+    public $_optionPrefix = 'sender_prestashop_';
+    private $defaultSettings = array();
+
+
     public function __construct() {
         $this->name = 'senderprestashop';
         $this->tab = 'Sender.net settings';
@@ -20,6 +24,55 @@ class SenderPrestashop extends Module
         $this->displayName = $this->l('Sender.net Integration');
         $this->description = $this->l('Sender.net email marketing integration for PrestaShop.');
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+        
+        $this->defaultSettings = array(
+            $_optionPrefix . 'module_active'  => 0,
+            $_optionPrefix . 'allow_forms'    => 0,
+            $_optionPrefix . 'allow_import'   => 0,
+            $_optionPrefix . 'allow_push'     => 0,
+            $_optionPrefix . 'customers_list' => '',
+            $_optionPrefix . 'forms_list'     => ''
+
+        );
+    }
+    
+    public function install() {
+        if (Shop::isFeatureActive()) {
+            Shop::setContext(Shop::CONTEXT_ALL);
+        }
+
+        if (parent::install())
+        {
+            
+            foreach($this->defaultSettings as $defaultSettingKey => $defaultSettingKey)
+            {
+                if(!Configuration::updateValue($defaultSettingKey, $defaultSettingKey))
+                {
+                    return false;
+                }
+            }
+            
+        }
+        
+        return true;
+    }
+    
+    public function uninstall()
+    {
+        if (parent::uninstall())
+        {
+            
+            foreach($this->defaultSettings as $defaultSettingKey => $defaultSettingKey)
+            {
+                if(!Configuration::deleteByName($defaultSettingKey))
+                {
+                    return false;
+                }
+            }
+            
+        }
+
+        return true;
     }
 
 }
