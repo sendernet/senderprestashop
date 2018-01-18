bc33acd34eff3c33a1ed7c4facc87da63db3c273
Augustas <augustas@sender.lt>
2018-01-18 13:46:38 +0200

    Added construct function

--- senderprestashop.php
+++ senderprestashop.php
@@ -6,6 +6,20 @@ if (!defined('_PS_VERSION_')) {
  
 class SenderPrestashop extends Module
 {
+    public function __construct() {
+        $this->name = 'senderprestashop';
+        $this->tab = 'Sender.net settings';
+        $this->version = '1.0.0';
+        $this->author = 'Sender.net';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+        $this->bootstrap = true;
 
+        parent::__construct();
+
+        $this->displayName = $this->l('Sender.net Integration');
+        $this->description = $this->l('Sender.net email marketing integration for PrestaShop.');
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+    }
 
 }
